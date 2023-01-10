44a09ed4e8c5e611bc1537d2c5a82e9fced9a69e
babitamagarjavra <122277120+babitamagarjavra@users.noreply.github.com>
2023-01-10 11:18:44 +0545

    Add files via upload
    validation is added and is working

--- action.php
+++ action.php
@@ -1,64 +1,36 @@
 <?php
 
-  /* if the request method post cha bhaney matra , function call garyo
+$json_data = fileInput();
 
-ani teslai json file ma write garyo*/
+if ((gettype(json_decode($json_data, true))) != 'NULL') 
+{
+  file_put_contents("formdata.json", $json_data);
+  echo $json_data;
+} 
+else 
+{
+  echo $json_data;
+}
 
-//   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $json_data = fileInput();
+function fileInput()
+{
+  $current_data = file_get_contents('formdata.json');
+  $decoded_data = json_decode($current_data, true);
+  $data_add = array(
+    'Product_name' => filter_input(INPUT_POST, 'pname', FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^([A-Za-z])*$/"))),
+    'Quantity' => filter_input(INPUT_POST, 'qty', FILTER_VALIDATE_INT),
+    'Rate' => filter_input(INPUT_POST, 'rate', FILTER_VALIDATE_FLOAT)
+  );
 
-    file_put_contents("formdata.json", $json_data);
-
-    echo $json_data;
-    
-
-  //defining function
-
-//   class Product{
-
-    function fileInput()
-
+  if ($data_add['Product_name'] != false && $data_add['Quantity'] != false && $data_add['Rate'] != false) 
   {
-
-    $current_data = file_get_contents('formdata.json');
-
-    $decoded_data = json_decode($current_data, true);
-
-    $data_add = array(
-
-        'Product_name' => filter_input(INPUT_POST,'pname',FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([A-Za-z0-9])*$/"))),
-        'Quantity' => filter_input(INPUT_POST,'qty',FILTER_VALIDATE_INT),
-        'Rate' => filter_input(INPUT_POST,'rate',FILTER_VALIDATE_FLOAT)
-    );
-
-    // if ($data_add['Product_name']='false')
-    // {
-    //     $message='please enter alphabets and numbers only in Product name field';
-    // }
-    // elseif ($data_add['Quantity']='false')
-    // {
-    //     $message='please enter intergers only in Quantity field';
-    // }
-    // elseif ($data_add['Rate']='false')
-    // {
-    //     $message='please enter float only in Rate field';
-    // }
-    // else{
     $decoded_data[] = $data_add;
-
     $updated_data = json_encode($decoded_data);
-    // }
-    // if (isset($message))
-    // {
-    //     return $message;
-    // }
-    // else
-    // {
     return $updated_data;
-    }
-  
-
-// }
-
-  ?>
+  } 
+  else 
+  {
+    $message = 'please enter correct format in the fields';
+    return $message;
+  }
+}
--- form.js
+++ form.js
@@ -1,53 +1,63 @@
 $(document).ready(function () {
 
   $("#form").submit(function (e) {
-
+  
     e.preventDefault();
-
+  
     var form = $(this);
 
-    
-
-    $.ajax({
-
+    $.ajax(
+    { 
       type: "POST",
-
       url: 'action.php',
+      data: form.serialize(), 
+      success: function (response) 
+      { 
+        function check(){
+        try {
+        JSON.parse(response);
+        return true;
+        } catch (error) {
+        return false;
+        }
+        }
 
-      data: form.serialize(), // serializes the form's elements.
+        value=check();
+        if(value==true)
+        {
+          var output = JSON.parse(response);
+          alert('data saved');
+          displaytable();
+        }
+        else{
+          alert(response);
+        }
 
-      success: function (response) {
-
-        // alert('data saved');
-        // if (typeOf(response)=='array')
-        // {
-        var output = JSON.parse(response);
-        displaytable();
-        
-        // else
-        // {
-        //   alert (response);
-        // }
         function displaytable()
         {
           $('#myTable').empty();
+          var total = 0;
+
           var heading = $('<tr><th>Product Name</th><th>Quantity</th><th>Rate</th><th>Price</th></tr>');
-          $('#myTable').append(heading);
-          for (var i = 0; i < output.length; i++) {
-            var row = $('<tr><td>' + output[i].Product_name + '</td><td>' + output[i].Quantity
-              + '</td><td>' + output[i].Rate + '</td><td>' + output[i].Quantity * output[i].Rate +
-              '</td></tr>');
+                        $('#myTable').append(heading);
+
+          for (var i = 0; i < output.length; i++) 
+          {
+           var row = $('<tr><td>' + output[i].Product_name + '</td><td>' + output[i].Quantity
+                     + '</td><td>' + output[i].Rate + '</td><td>' + output[i].Quantity * output[i]
+                     .Rate +'</td></tr>');
             $('#myTable').append(row);
-          }// show response from the php script.
-        }
-        
+          }
+          for (var i=0; i< output.length; i++){
+            total = total+ (output[i].Quantity * output[i].Rate);
+          }
+          var totalrow= $('<tr> <th colspan=3>Total</th><td>'+total+'</td></tr>');
+          $('#myTable').append(totalrow);
+          }
+          
       }
-
-
-
-    })
-
-  })
-
-});
-
+    });
+  
+    });
+  
+  });
